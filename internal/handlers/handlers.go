744d9da8863aafe01cc5f9cff844b238cff2395e
Malav Treasurer <malav.treasurer@gmail.com>
2024-06-20 17:07:40 +0530

    Update handlers.go

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,13 +2,19 @@ package handlers
 
 import (
 	"database/sql" // Add this line
+	"log"
+	"os"
+	"strings"
+	"time"
 
 	database "jobscheduler/internal/db"
 	"jobscheduler/internal/models"
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/ssh"
 )
 
 // var user = "jobsadmin"
@@ -106,4 +112,110 @@ func DeleteJob(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "Job deleted successfully"})
 
+}
+
+func RunJob(c *gin.Context) {
+	jobName := c.Param("job_name")
+
+	job, err := database.GetJobByName(jobName)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Job not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	} else {
+		hosts := strings.Split(strings.Trim(job.Hosts, "[]"), ",")
+		var host_list []string
+		for _, host := range hosts {
+			host_list = append(host_list, strings.TrimSpace(host))
+		}
+		var wg sync.WaitGroup
+		wg.Add(len(host_list))
+		start_time := time.Now()
+		// err = database.InsertExecutionDetails(jobName, start_time, time.Time{}, "In Progress")
+		// if err != nil {
+		// 	c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		// 	return
+		// }
+		job_failed := false
+		for _, host := range host_list {
+			//hostStr := string(host)
+			go func(host string) {
+				defer wg.Done()
+
+				key, err := os.ReadFile("path-to-pem-file") // replace with your private key path
+				// log.Printf("Key is present for host %s", host)
+				if err != nil {
+					job_failed = true
+					log.Printf("unable to read private key: %v", err)
+					return
+				}
+
+				signer, err := ssh.ParsePrivateKey(key)
+				if err != nil {
+					job_failed = true
+					log.Printf("unable to parse private key: %v", err)
+					return
+				}
+				config := &ssh.ClientConfig{
+					User: "user", // replace with your username
+					Auth: []ssh.AuthMethod{
+						ssh.PublicKeys(signer),
+					},
+					HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+					Timeout:         30 * time.Second,
+				}
+				client, err := ssh.Dial("tcp", host+":22", config)
+				if err != nil {
+					// c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+					job_failed = true
+					log.Printf("Error occurred while SSH to VM: %v", err)
+					return
+				}
+				defer client.Close()
+				session, err := client.NewSession()
+				if err != nil {
+					// c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+					job_failed = true
+					log.Printf("Error occurred while creating new session: %v", err)
+					return
+				}
+				defer session.Close()
+
+				// replace 'command-to-run-job' with the command to run your job
+				err = session.Run(job.JobCommand)
+				if err != nil {
+					// c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+					job_failed = true
+					log.Printf("Error occurred while running job: %v", err)
+					return
+				}
+			}(host)
+		}
+		wg.Wait()
+		end_time := time.Now()
+		if !job_failed {
+			c.JSON(http.StatusOK, gin.H{"message": "Job run successfully"})
+			err = database.InsertExecutionDetails(jobName, start_time, end_time, "Success")
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": "Job failed"})
+			err = database.InsertExecutionDetails(jobName, start_time, end_time, "Failed")
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+				return
+			}
+		}
+
+	}
+}
+
+func GetExecutionHistory(c *gin.Context) {
+	jobs := database.GetExecutionHistory()
+	c.JSON(http.StatusOK, jobs)
 }
